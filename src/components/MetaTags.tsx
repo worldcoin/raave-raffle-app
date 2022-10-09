258c4f09e4151515321420d3ace3ba7793c9d279
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-10-09 12:08:27 -0500

    wip

---
+++ src/components/MetaTags.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import coverImg from '@images/cover.jpg'
+
+const MetaTags = () => {
+	const meta = {
+		title: "We're giving away 5 rAAVE Bogotá tickets!",
+		image: coverImg.src,
+		url: 'https://raave.worldcoin.org',
+		description:
+			"We've partnered with Lens to give away 5 rAAVE Bogotá tickets to WorldID-verified Lens profiles. See you on the dance floor! 🕺",
+	}
+
+	return (
+		<Head>
+			<title>{meta.title}</title>
+			<meta name="title" content={meta.title} />
+			<meta name="description" content={meta.description} />
+
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content={meta.url} />
+			<meta property="og:title" content={meta.title} />
+			<meta property="og:description" content={meta.description} />
+			<meta property="og:image" content={meta.url + meta.image} />
+
+			<meta property="twitter:card" content="summary_large_image" />
+			<meta property="twitter:url" content={meta.url} />
+			<meta property="twitter:title" content={meta.title} />
+			<meta property="twitter:description" content={meta.description} />
+			<meta property="twitter:image" content={meta.url + meta.image} />
+		</Head>
+	)
+}
+
+export default MetaTags
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { FC, useCallback } from 'react'
 import Button from '@/components/Button'
+import MetaTags from '@/components/Metatags'
 import { useToggle } from '@/hooks/useToggle'
 import { useAccount, useNetwork } from 'wagmi'
 import raavePoster from '@images/raave-poster.jpeg'
@@ -10,69 +11,71 @@ import SubscribeModal from '@/components/SubscribeModal'
 
 const Home: FC = () => {
 	const { chain } = useNetwork()
-	const { isConnected, address } = useAccount()
-	const subscribeModal = useToggle()
 	const successModal = useToggle()
+	const subscribeModal = useToggle()
+	const { isConnected } = useAccount()
 
 	const handleSuccess = useCallback(() => {
-		subscribeModal.toggleOff()
 		successModal.toggleOn()
+		subscribeModal.toggleOff()
 	}, [subscribeModal, successModal])
 
 	return (
-		<div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-			<div className="absolute top-6 right-6">
-				<ConnectWallet />
-			</div>
+		<>
+			<MetaTags />
+			<SubscribeModal modalState={subscribeModal} onSuccess={handleSuccess} />
+			<SuccessModal modalState={successModal} />
+			<div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+				<div className="absolute top-6 right-6">
+					<ConnectWallet />
+				</div>
 
-			<div className="flex space-x-16">
-				<Image
-					src={raavePoster}
-					placeholder="blur"
-					alt=""
-					height={320}
-					width={250}
-					className="rounded-lg mb-8"
-				/>
-				<div>
-					<h1 className="text-4xl font-sora">
-						rAAVE Bogotá 2022 <span className="font-bold">humans-only raffle</span>
-					</h1>
-					<p className="text-center text-neutral-300">By 👻 rAAVE x 🍃 Lens x 🪩 Worldcoin</p>
-					<div className="mt-8">
-						<p className="text-center">
-							We&apos;re raffling <span className="text-3xl font-bold">5x rAAVE tix 🎟</span>
-						</p>
-						<p className="mt-12">It&apos;s super easy to participate:</p>
-						<ol className="list-decimal pl-8">
-							<li className="pb-4 pt-4">
-								<a
-									href="https://human.withlens.app"
-									target="_blank"
-									className="text-indigo-400"
-									rel="noreferrer"
-								>
-									Verify
-								</a>{' '}
-								your Lens profile with Worldcoin (if you haven&apos;t already)
-							</li>
-							<li className="pb-4">Subscribe below 👇</li>
-							<li className="pb-4">💃🕺 like there&apos;s no tomorrow</li>
-						</ol>
-					</div>
-					<div className="mt-8 flex justify-center">
-						<ConnectWallet visibility="not_connected" />
+				<div className="flex space-x-16">
+					<Image
+						width={250}
+						height={320}
+						src={raavePoster}
+						placeholder="blur"
+						alt="rAAVE Bogotá poster"
+						className="rounded-lg mb-8"
+					/>
+					<div>
+						<h1 className="text-4xl font-sora">
+							rAAVE Bogotá 2022 <span className="font-bold">humans-only raffle</span>
+						</h1>
+						<p className="text-center text-neutral-300">By 👻 rAAVE x 🍃 Lens x 🪩 Worldcoin</p>
+						<div className="mt-8">
+							<p className="text-center">
+								We&apos;re raffling <span className="text-3xl font-bold">5x rAAVE tix 🎟</span>
+							</p>
+							<p className="mt-12">It&apos;s super easy to participate:</p>
+							<ol className="list-decimal pl-8">
+								<li className="pb-4 pt-4">
+									<a
+										target="_blank"
+										rel="noreferrer"
+										className="text-indigo-400"
+										href="https://human.withlens.app"
+									>
+										Verify
+									</a>{' '}
+									your Lens profile with Worldcoin (if you haven&apos;t already)
+								</li>
+								<li className="pb-4">Subscribe below 👇</li>
+								<li className="pb-4">💃🕺 like there&apos;s no tomorrow</li>
+							</ol>
+						</div>
+						<div className="mt-8 flex justify-center">
+							<ConnectWallet visibility="not_connected" />
 
-						{isConnected && !chain.unsupported && (
-							<Button onClick={() => subscribeModal.toggleOn()}>Subscribe now</Button>
-						)}
+							{isConnected && !chain.unsupported && (
+								<Button onClick={() => subscribeModal.toggleOn()}>Subscribe now</Button>
+							)}
+						</div>
 					</div>
 				</div>
 			</div>
-
-			<SubscribeModal modalState={subscribeModal} onSuccess={handleSuccess} />
-			<SuccessModal modalState={successModal} />
-		</div>
+		</>
 	)
 }
 
